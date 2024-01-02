54f0eabe8395d4a78a569591b5944824a9e44459
Razalp <razalp0012300@gmail.com>
2024-01-02 12:46:20 +0530

    start to create frortend

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { default: axios } = require("axios");
 
 const app = express();
 app.use(express.json());
@@ -8,11 +9,16 @@ app.use(cors({ origin: true }));
 app.post("/authenticate", async (req, res) => {
   const { username } = req.body;
   try {
-    
+    const response=await axios.put(
+        'https://api.chatengine.io/users/',
+        {username:username, secret:username,first_name:username},
+        {headers:{'privat-key':"877266c9-1b56-42e2-9aaa-9d54265e3523"}}
+    )
+    return res.status(response.status).json(response.data)
   } catch (error) {
     
   }
-  return res.json({ username: username, secret: "sha256..." });
+  return res.json({ username: username, secret: "razal...." });
 });
 
 app.listen(3001);
